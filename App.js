158c6a0f4df0149df0738e30e4460cd14ef80afe
juancaste <juandiegocastellanos7@gmail.com>
2022-09-24 15:04:31 -0500

    first commit exercice

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TextInput, Dimensions, TouchableWithoutFeedback, ScrollView } from 'react-native';
 
 export default function App() {
+  const [ text, setText ] = useState('Uno en el parcial')
+  const [ submit, setSubmit ] = useState('')
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ styles.container }>
+      <ScrollView style={ styles.scrollView}>
+        <Text>Texto: { submit }</Text> 
+        <Text>Texto: { submit }</Text>
+        <Text>Texto: { submit }</Text>
+        <Text>Texto: { submit }</Text>
+        <Text>Texto: { submit }</Text>
+        <Text>Texto: { submit }</Text>
+        <Text>Texto: { submit }</Text>
+        <Text>Texto: { submit }</Text>
+        <Text>Texto: { submit }</Text>
+        <Text>Texto: { submit }</Text>
+        <TextInput style={ styles.input }
+            placeholder='Write here'
+            onChangeText={ Texto => setText(Texto) }
+            defaultValue={ text }
+        />
+        <TouchableWithoutFeedback
+            underlayColor={ '#999' }
+            activeOpacity={0.2}
+            onPress={() => {
+            setSubmit(text)
+            alert('Mentiras, su nota es cinto')
+            }}><Text>Accept</Text>
+        </TouchableWithoutFeedback>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  scrollView: {
+    width: Dimensions.get('window').width
   },
 });
